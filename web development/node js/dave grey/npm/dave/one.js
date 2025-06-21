7f5cebdb86b227a3aee0d8974cb154c93231242f
Vishnu14052002 <vishnug14052002@gmail.com>
2025-06-21 01:35:03 +0100

    commit

--- web development/node js/dave grey/npm/dave/one.js
+++ web development/node js/dave grey/npm/dave/one.js
@@ -1,1 +1,20 @@
-console.log('hello')
+const { format } = require('date-fns');
+const { v4: uuid } = require('uuid');
+
+const fs = require('fs');
+const path = require('path');
+const fsPromise = require('fs').promises;
+
+const logEvents = async(message) => {
+    const dateTime = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}`;
+    const logItem = `${dateTime}\t${uuid()}\t${message}`
+    console.log(logItem);
+    try{
+        await fsPromise.appendFile(path.join(__dirname, 'eventLog.txt'), logItem); 
+    }catch(err){
+        console.log(err);
+    }
+}
+
+
+module.exports = logEvents;
