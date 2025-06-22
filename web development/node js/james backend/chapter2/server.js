59cc86ac34d33cc035c867ce95b53f63589c914a
Vishnu14052002 <vishnug14052002@gmail.com>
2025-06-22 03:18:10 +0100

    commit

--- web development/node js/james backend/chapter2/server.js
+++ web development/node js/james backend/chapter2/server.js
@@ -2,6 +2,36 @@ const express = require('express');
 const app = express();
 const PORT = 8383;
 
+let data = ['vishnu']
+
+app.use(express.json())
+
 console.log('hello world')
+
+app.get('/' , (req, res) => {
+    res.send(`
+            <body>
+                <h1>data </h1>
+                <p>${JSON.stringify(data)}</p>
+            </body
+        `);
+});
+
+app.get('/dashboard', (req, res) => {
+    console.log('i hit /dashboard url')
+    res.send('<h1>dashboard</h1>')
+});
+
+app.get('/api/data', (req, res) => {
+    console.log('this is end point data');
+    res.send(data);
+});
+
+app.post('/api/data', (req, res) => {
+    const newEntry = req.body;
+    res.send(201);
+    data.push(newEntry.name);
+    console.log(newEntry);
+})
 
 app.listen(PORT , () => {console.log(`server started on ${PORT}`)});
