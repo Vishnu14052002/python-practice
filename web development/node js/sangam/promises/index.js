98a4df8460791eb009db73ff94c3a2bd4ed13de4
Vishnu14052002 <vishnug14052002@gmail.com>
2025-06-27 01:37:20 +0100

    promises

---
+++ web development/node js/sangam/promises/index.js
@@ -0,0 +1,33 @@
+
+
+function delay(time){
+    return new Promise((resolve, reject) => {
+        setTimeout(resolve, time);
+    })
+}
+
+console.log(`promise `);
+
+delay(2000).then(() => console.log('after 2 seconds promise resolved'))
+
+console.log('end');
+
+
+
+function divide(num1, num2) {
+    return new Promise((resolve, reject) => {
+        if(num2 ===0){
+            reject('cannot divide a number by 0');
+        }
+        else{
+            resolve(num1/num2);
+        }
+    })
+}
+
+
+divide(1,2).then((resolve) => {
+    console.log(resolve)
+}).catch((reject) => {
+    console.log(reject)
+})
