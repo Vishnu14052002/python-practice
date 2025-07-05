024af489c3baa633ce01668f2edd4c7376318f01
Vishnu14052002 <vishnug14052002@gmail.com>
2025-07-06 00:55:42 +0100

    commit

--- web development/node js/my practice/jwt/server.js
+++ web development/node js/my practice/jwt/server.js
@@ -16,18 +16,13 @@ app.get('/posts', (req, res) => {
     res.send(posts)
 })
 
-
 app.post('/login', (req, res) => {
-    const username = req.body.name;
-    console.log(username);
-    if(username){
-        res.status(200).json({
-            'message' : 'success'
-        })
-    }
+    const name = req.body.name;
+    console.log('login page', name)
 })
 
 
 app.listen(PORT, () => {
     console.log('server is running');
 })
+
