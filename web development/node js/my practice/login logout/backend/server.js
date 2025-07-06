08f98c812e34713450f2c65c87a60d05e75093b9
Vishnu14052002 <vishnug14052002@gmail.com>
2025-07-07 00:38:58 +0100

    registration successfull

--- web development/node js/my practice/login logout/backend/server.js
+++ web development/node js/my practice/login logout/backend/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 require('dotenv').config();
 const app = express();
 app.use(express.json());
+
+const saltRound = 10;
 
 mongoose.connect('mongodb+srv://vishnug14052002:vishnu24@cluster0.guzcoui.mongodb.net/')
 .then(() => {console.log('database connected successfully')})
@@ -14,12 +17,32 @@ const usersSchema = new mongoose.Schema({
     password : String
 })
 
-const user_details = mongoose.model('users', usersSchema)
+const user_details = mongoose.model('user_details', usersSchema)
 
-app.post('/register', (req, res) => {
+app.post('/register', async (req, res) => {
     const {name, email, password} = req.body;
     console.log(name, email, password);
+    const salt = await bcrypt.genSalt(saltRound);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new user_details({
+        name,
+        email,
+        password: hashedPassword
+    });
+    
+    await newUser.save();
+
+
+    res.status(400).json({
+        'message' : 'registration successfull'
+    })
 })
+
+
+
+
+
 
 app.listen(process.env.PORT, () => {
     console.log('server is running');
