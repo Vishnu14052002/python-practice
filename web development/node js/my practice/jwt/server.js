f8134abcaab15d67b2d2dba2d426e5d7a02af18e
Vishnu14052002 <vishnug14052002@gmail.com>
2025-07-06 02:06:53 +0100

    commit

--- web development/node js/my practice/jwt/server.js
+++ web development/node js/my practice/jwt/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const app = express();
 require('dotenv').config();
 const PORT = 3000;
@@ -12,14 +13,35 @@ const posts = [
     }
 ]
 
-app.get('/posts', (req, res) => {
+app.get('/posts', authentication, (req, res) => {
+    console.log(req.user)
     res.send(posts)
 })
 
 app.post('/login', (req, res) => {
     const name = req.body.name;
     console.log('login page', name)
+    const token = jwt.sign({name}, process.env.JWT_SECRET);
+    console.log(token);
+    res.json({ token });
 })
+
+// middleware
+
+function authentication(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1]; // Bearer TOKEN
+    console.log(token)
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403);
+        console.log('success')
+        req.user = user;
+        next();
+    });
+    next();
+}
+
 
 
 app.listen(PORT, () => {
