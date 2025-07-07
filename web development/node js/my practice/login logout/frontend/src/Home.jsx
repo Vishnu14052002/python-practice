5493b83d3cf1a3f11618e913b9fadddaabb32dbe
Vishnu14052002 <vishnug14052002@gmail.com>
2025-07-07 02:10:45 +0100

    commit

---
+++ web development/node js/my practice/login logout/frontend/src/Home.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+
+const Home = () => {
+    const [loginClick, setLoginClick] = useState(false);
+    const [registerClick, setRegisterClick] = useState(false);
+    const [loginUserName, setLoginUserName] = useState('');
+    const [loginPassword, setLoginPassword] = useState('');
+    const [registerUserName, setRegisterUserName] = useState('');
+    const [registerEmail, setRegisterEmail] = useState('');
+    const [registerPassword, setRegisterPassword] = useState('');
+    // console.log(registerUserName)
+
+    function handleLogin(e){
+        e.preventDefault();
+    }
+
+    function handleRegister(e){
+        e.preventDefault();
+    }
+
+    return (
+        <div>
+            <h1>to do app</h1>
+            <button onClick={() => {setLoginClick(true),setRegisterClick(false)}}>login</button>
+            <button onClick={() => {setLoginClick(false),setRegisterClick(true)}}>register</button>
+            <br></br><br></br>
+            {loginClick && (
+                <form>
+                    <input onChange={(e) => setLoginUserName(e.target.value)} placeholder='enter email'></input><br></br><br></br>
+                    <input onChange={(e) => setLoginPassword(e.target.value)} type='password' placeholder='enter password'></input><br></br><br></br>
+                    <button onClick={handleLogin}>login</button>
+                </form>
+            )}
+            {registerClick && (
+                <form>
+                    <input onChange={(e) => setRegisterUserName(e.target.value)} placeholder='enter name'></input><br></br><br></br>
+                    <input onChange={(e) => setRegisterEmail(e.target.value)} placeholder='enter email'></input><br></br><br></br>
+                    <input onChange={(e) => setRegisterPassword(e.target.value)} placeholder='enter password'></input><br></br><br></br>
+                    <button onClick={handleRegister}>register</button>
+                </form>
+            )}
+            
+        </div>
+    )
+}
+
+export default Home
--- web development/node js/my practice/login logout/frontend/src/main.jsx
+++ web development/node js/my practice/login logout/frontend/src/main.jsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Home from './Home.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+
+const router = createBrowserRouter([
+  {
+    path : '/',
+    element : <Home />
+  }
+])
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <>
     <App />
-  </StrictMode>,
+    <RouterProvider router={router}/>
+  </>
 )
