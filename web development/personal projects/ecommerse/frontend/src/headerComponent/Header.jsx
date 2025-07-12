8a96fd0d56524e85b9332190084f37ab824ba685
Vishnu14052002 <vishnug14052002@gmail.com>
2025-07-12 03:01:54 +0100

    header commit

--- web development/personal projects/ecommerse/frontend/src/headerComponent/Header.jsx
+++ web development/personal projects/ecommerse/frontend/src/headerComponent/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './header.css'
 import '../../src/assets/logo.png'
 import { CiLocationOn } from "react-icons/ci";
@@ -11,15 +11,15 @@ import { IoCloseSharp } from "react-icons/io5";
 const Header = () => {
     const userAdderss = 'chelmsford';
     const userName = 'vishnu';
-    const userLoggedin = true;
+    const [userLoggedin, setUserLoggedIn] = useState(false);
     const cartItemNumber = 2;
-    const showRegister = false;
-    const showLogin = true;
+    const [showRegister, setShowRegister] = useState(false);
+    const [showLogin, setShowLogin] = useState(false);
     const login = (
         <div className="loginMainModel">
           <div className="loginSuma">
             <h2 className="loginModel">Login</h2>
-            <IoCloseSharp className="loginModelCloseIcon" />
+            <IoCloseSharp onClick={() => {setShowLogin(false)}} className="loginModelCloseIcon" />
           </div>
           <form>
             <input placeholder="Email or phone number" /><br />
@@ -28,7 +28,7 @@ const Header = () => {
           </form>
       
           <p>or</p>
-          <h4>Register</h4>
+          <h4 onClick={() => {setShowLogin(false);setShowRegister(true)}}>Register</h4>
         </div>
       );
     
@@ -36,7 +36,7 @@ const Header = () => {
         <div className="RegisterMainModel">
             <div className="registerSuma">
                 <h2 className="RegisterModel">Register</h2>
-                <IoCloseSharp className='RegisterModelCloseIcon'/>
+                <IoCloseSharp onClick={() => {setShowRegister(false)}} className='RegisterModelCloseIcon'/>
             </div>
             <form>
                 <input placeholder='Name' /><br />
@@ -49,7 +49,7 @@ const Header = () => {
             </form>
 
             <p>or</p>
-            <h4>Login</h4>
+            <h4 onClick={() => {setShowLogin(true);setShowRegister(false)}}>Login</h4>
         </div>
     );
     
@@ -85,7 +85,7 @@ const Header = () => {
                     </>
                 ): 
                 <>
-                    <h4>login / register</h4>
+                    <h4 onClick={() => {setShowLogin(true)}}>login / register</h4>
                 </>
                 }
             </div>
@@ -96,9 +96,7 @@ const Header = () => {
             </div>
         </div>
 
-        <div className="authWrapper">
-            {showLogin ? login : register}
-        </div>
+
 
         <div className="authWrapper">
             {showLogin && login}
