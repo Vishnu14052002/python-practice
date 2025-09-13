9823c9957b35a6d1afb86862eb68eea6162dd2d6
Vishnu14052002 <vishnug14052002@gmail.com>
2025-09-13 23:40:43 +0100

    cs

--- web development/personal projects/ecommerse/frontend/src/headerComponent/Header.jsx
+++ web development/personal projects/ecommerse/frontend/src/headerComponent/Header.jsx
@@ -93,7 +93,7 @@ const Header = () => {
                 </>
                 }
             </div>
-{/* commensss*/}s
+{/* commes*/}s
             <div className="cart">
                 <p className='cartItemNumber'>{cartItemNumber}</p>
                 <CgShoppingCart className='carticon'/>
