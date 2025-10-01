514caece63fe7710c91e680cf6f7dae9f8d6693c
Vishnu14052002 <vishnug14052002@gmail.com>
2025-10-01 23:39:59 +0100

    commit

--- leetcode problems/array/merged_sorted_88.py
+++ leetcode problems/array/merged_sorted_88.py
@@ -1,11 +1,8 @@
 class Solution(object):
     def merge(self, nums1, m, nums2, n):
-        d_nums1 = nums1
+        d_nums1 = nums1[:m]
         print(d_nums1)
-        for i in range(len(d_nums1)):
-            if d_nums1[i] == 0:
-                d_nums1.pop(i)
-        print(d_nums1)       
+   
 
 
 
