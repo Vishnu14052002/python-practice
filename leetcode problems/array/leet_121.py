42b4bffea785c70c89ff5f547601ce28b63aa706
Vishnu14052002 <vishnug14052002@gmail.com>
2025-10-01 01:10:57 +0100

    commits

--- leetcode problems/array/leet_121.py
+++ leetcode problems/array/leet_121.py
@@ -1,9 +1,13 @@
 class Solution(object):
     def maxProfit(self, prices):
-        print('hello world')
+        min = prices[0]
+        profit = 0
+        for i in range(1, len(prices)):
+            print(prices[i])
+
+            
 
 
 
 obj = Solution()
-obj.maxProfit(23)
-print('hello')
+obj.maxProfit([7,1,5,3,6,4])
