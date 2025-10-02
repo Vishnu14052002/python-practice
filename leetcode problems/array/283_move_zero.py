83e316c36224dd2477002d7032e55bca05a95d0d
Vishnu14052002 <vishnug14052002@gmail.com>
2025-10-03 00:03:21 +0100

    commits

--- leetcode problems/array/283_move_zero.py
+++ leetcode problems/array/283_move_zero.py
@@ -1,23 +1,15 @@
 class Solution(object):
     def moveZeroes(self, nums):
         x = 0
-        j = 1
-        length = len(nums)
-        if length == 1:
-            return nums
-        for i in range(1, len(nums)):
+        for i in range(len(nums)):
             if nums[i] != 0:
                 nums[x] = nums[i]
-                x = x + 1
-                print(nums) 
-
-        for i in range(x, len(nums)):
-            nums[x] = 0
-            x = x + 1
-            print(nums)
+                x += 1
+                print(i,x)
+            # print(nums)
 
 
 
 obj = Solution()
-nums = [1]
+nums = [0,1,0,3,12]
 obj.moveZeroes(nums)
