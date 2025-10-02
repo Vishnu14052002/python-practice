61056a1e210a0b0e876a69e2508e21263dfafb47
Vishnu14052002 <vishnug14052002@gmail.com>
2025-10-02 23:35:37 +0100

    commits

--- leetcode problems/array/merged_sorted_88.py
+++ leetcode problems/array/merged_sorted_88.py
@@ -1,13 +1,19 @@
 class Solution(object):
     def merge(self, nums1, m, nums2, n):
         count = 0
-           
+        for i in range(m, (m+n)):
+            nums1[i] = nums2[count]
+            count = count + 1
+        print(nums1)
+        nums1.sort()
+        
 
 
 
-nums1 = [1,2,3,0,0,0]
-m = 3
-nums2 = [2,5,6]
+
+nums1 = [-1,0,0,3,3,3,0,0,0]
+m = 6
+nums2 = [1,2,2]
 n = 3
 obj = Solution()
 obj.merge(nums1, m, nums2, n)
