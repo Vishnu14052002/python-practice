1aeadd22f41219ccb9700491cd4314b9a5c221e2
Vishnu14052002 <vishnug14052002@gmail.com>
2025-10-04 16:00:11 +0100

    commits

--- leetcode problems/array/485_max_count.py
+++ leetcode problems/array/485_max_count.py
@@ -2,29 +2,14 @@ class Solution(object):
     def findMaxConsecutiveOnes(self, nums):
         count = 0
         max_count = 0
-        for i in range(len(nums)):
-            if nums[i] == 1:
-                count += 1
-                print(nums[i])
-                print(count)
-            
-        
-            elif max_count < count:
-                print(nums[i])
-                max_count = count
-                count = 0
 
-            if max_count < count:
-                print('count inside loop',count)
-            elif count < max_count:
-                print('max_count inside loop',max_count)
-            
-        if max_count < count:
-            print('count',count)
-        elif count < max_count:
-            print('max_count',max_count)
+        for i in nums:
+            print(i)
+
+
+
         
 
 obj = Solution()
-nums = [0]
+nums = [1,1,0,0,0,1,1,1,0]
 obj.findMaxConsecutiveOnes(nums)
