f74acfa518cb80af3ddfeb7cb674fd5245241911
Vishnu14052002 <vishnug14052002@gmail.com>
2025-10-04 14:11:31 +0100

    commits

--- leetcode problems/array/485_max_count.py
+++ leetcode problems/array/485_max_count.py
@@ -6,10 +6,17 @@ class Solution(object):
             if nums[i] == 1:
                 count += 1
                 print(count)
+            
         
             else:
                 max_count = count
                 count = 0
+            
+            if i == len(nums)-1:
+                if count < max_count:
+                    print("max",max_count)
+                else:
+                    print("count",count)
         
         print(max_count)
 
