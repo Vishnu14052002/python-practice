5a7d058a00287b904b446fc0148dcb24d0883c74
Vishnu14052002 <vishnug14052002@gmail.com>
2025-10-04 17:10:50 +0100

    commits

--- leetcode problems/array/485_max_count.py
+++ leetcode problems/array/485_max_count.py
@@ -5,11 +5,25 @@ class Solution(object):
 
         for i in nums:
             print(i)
+            if i == 1:
+                print('run')
+                count += 1
+            
+            elif i == 0:
+                print('runn')
+                if max_count < count:
+                    max_count = count
+                    count = 0
+                else:
+                    count = 0
+        
+        print(max(count, max_count))
+            
 
 
 
         
 
 obj = Solution()
-nums = [1,1,0,0,0,1,1,1,0]
+nums = [1,1,0,0,0,1,1,1,0,1,1,1,1,1,1,1]
 obj.findMaxConsecutiveOnes(nums)
