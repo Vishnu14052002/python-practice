930056c9ea4b66b6d7e3599857ca483ac5b333f5
Vishnu14052002 <vishnug14052002@gmail.com>
2025-10-06 16:43:39 +0100

    commit

--- leetcode problems/recursion/sumOfArray.py
+++ leetcode problems/recursion/sumOfArray.py
@@ -1,7 +1,10 @@
-def sumOfArray(arr):
-    if arr == 0: return 0
+def sumOfArray(n, arr):
+    if n == 0: return arr[0]
+    return arr[n] + sumOfArray(n-1, arr)
     
 
 
 arr = [1,3,5,2,1,3]
-sumOfArray(arr)
+leng = len(arr) - 1
+value = sumOfArray(leng, arr)
+print(value)
