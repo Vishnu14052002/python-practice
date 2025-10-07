969f7b3ad92977fc7e84cb5e94f022a72d4bdcc0
Vishnu14052002 <vishnug14052002@gmail.com>
2025-10-07 21:54:48 +0100

    commits

--- leetcode problems/search and sort/bubble_sort.py
+++ leetcode problems/search and sort/bubble_sort.py
@@ -1,8 +1,13 @@
-arr = [4, 6, 3, 1, 9, 7, 2]
-# arr = [1,2,3,4]
+# arr = [4, 6, 3, 1, 9, 7, 2]
+arr = [2,1,3,4,5,6,7]
 
 for i in range(len(arr) - 1):
+    isSwap = False
     for j in range(len(arr)- 1 - i):
         if arr[j] > arr[j+1]:
             arr[j], arr[j+1] = arr[j+1], arr[j]
+            isSwap = True
+            print(i, j)
+    if isSwap == False:
+        break
     print(arr)
