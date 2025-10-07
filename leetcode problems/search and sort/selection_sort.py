e1050c73d4efba29cdc8110d32469a936ea09d14
Vishnu14052002 <vishnug14052002@gmail.com>
2025-10-07 22:50:59 +0100

    selection sort code

--- leetcode problems/search and sort/selection_sort.py
+++ leetcode problems/search and sort/selection_sort.py
@@ -1,8 +1,16 @@
 class Solution(object):
     def sortArray(self, nums):
-        print(nums)
+        for i in range(len(nums)):
+            min = i
+            for j in range(i+1, len(nums)):
+                if nums[min] > nums[j]:
+                    min = j
+            nums[i], nums[min] = nums[min], nums[i]
+        return nums
+            # return nums
 
 
 obj = Solution()
-nums = [5,2,3,1]
-obj.sortArray(nums)
+nums = [5,1,1,2,0,0]
+result = obj.sortArray(nums)
+print(result)
