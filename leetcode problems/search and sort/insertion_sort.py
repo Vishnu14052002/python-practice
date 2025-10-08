340fe5b4b008dacb28b26aad14bc3910c4ec5ccf
Vishnu14052002 <vishnug14052002@gmail.com>
2025-10-08 23:55:35 +0100

    insertion commit

--- leetcode problems/search and sort/insertion_sort.py
+++ leetcode problems/search and sort/insertion_sort.py
@@ -6,7 +6,7 @@ def insertionSort(nums):
             nums[current], nums[prev] = nums[prev], nums[current]
             current -= 1
             prev -= 1
-            print(nums)
+    print(nums)
 
 
 nums = [3,2,1,5,9,6,10,4, -10, -20, 40, 199, -2000]
