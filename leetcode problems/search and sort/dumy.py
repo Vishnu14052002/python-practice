a2babf9e25c36885eae139bc43ef9ea8fc7ccbd0
Vishnu14052002 <vishnug14052002@gmail.com>
2025-10-10 11:17:05 +0100

    commits

--- leetcode problems/search and sort/dumy.py
+++ leetcode problems/search and sort/dumy.py
@@ -1,27 +1,8 @@
-class Solution(object):
-    def maxProfit(self, prices):
-        max_profit = 0
-        curr_profit = 0
-        if len(prices) <= 1:
-            return 0
-        if len(prices) <= 2:
-            max_profit = prices[1] - prices[0]
-            if max_profit < 0:
-                return 0
-            else:
-                return max_profit
-        elif len(prices) > 2:
-            for i in range(len(prices)- 1):
-                for j in range(i + 1, len(prices) ):
-                    print(i, j)
-                    curr_profit = prices[j] - prices[i]
-                    if curr_profit > max_profit:
-                        max_profit = curr_profit
-            return max_profit
-        if max_profit < 0:
-            return 0
-        
-obj = Solution()
-prices = [1,2,4]
-result = obj.maxProfit(prices)
-print(result)
+def merge(left, right):
+    i = 0
+    j = 0
+    while(len(left) > 0 and len(right) > 0)
+
+left = [2,5,3,1] 
+right = [9,6,4,8]
+merge(left, right)
