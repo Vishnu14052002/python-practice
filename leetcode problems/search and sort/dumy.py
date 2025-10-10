315985e413c455114c0eca7ed2a056e960af3d58
Vishnu14052002 <vishnug14052002@gmail.com>
2025-10-10 11:38:41 +0100

    merge commit

--- leetcode problems/search and sort/dumy.py
+++ leetcode problems/search and sort/dumy.py
@@ -1,8 +1,18 @@
 def merge(left, right):
     i = 0
     j = 0
-    while(len(left) > 0 and len(right) > 0)
+    new_arr = []
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            new_arr.append(left[i])
+            i += 1
+        else:
+            new_arr.append(right[j])
+            j += 1
+    new_arr = [*new_arr, *left[i:], *right[j:]]
+    return new_arr
 
-left = [2,5,3,1] 
-right = [9,6,4,8]
-merge(left, right)
+left = [2,5,9,10] 
+right = [1,3,6,8]
+result = merge(left, right)
+print(result)
