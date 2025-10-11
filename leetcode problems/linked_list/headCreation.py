ce49bbf6809eb02cc25af111bb647a28e26cf27e
Vishnu14052002 <vishnug14052002@gmail.com>
2025-10-11 13:29:30 +0100

    singly linked list iteration

---
+++ leetcode problems/linked_list/headCreation.py
@@ -0,0 +1,29 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class SinglyLL:
+    def __init__(self):
+        self.head = None
+    
+    def traversal(self):
+        if self.head is None:
+            print('There is nothing')
+        else:
+            s1 = self.head
+            while s1 is not None:
+                print(s1.data)
+                s1 = s1.next
+
+sl = SinglyLL()
+n1 = Node(10)
+sl.head = n1
+n2 = Node(20)
+n3 = Node(30)
+n4 = Node(40)
+n1.next = n2
+n2.next = n3
+n3.next = n4
+
+sl.traversal()
