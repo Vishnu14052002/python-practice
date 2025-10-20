e262067a5ccb9378ebd3d38131e4cf518ab22408
Vishnu14052002 <vishnug14052002@gmail.com>
2025-10-20 12:02:48 +0100

    linked commit

---
+++ leetcode problems/linked_list/206_reverse_linkedlist.py
@@ -0,0 +1,20 @@
+class Node():
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+    
+class LinkedList():
+    def __init__(self):
+        self.head = None
+
+    def addAtEnd(self, val):
+        
+
+    def traverse(self):
+        s1 = self.head
+        while self.head is not None:
+            print(s1.value)
+            s1 = s1.next
+    
+
+
