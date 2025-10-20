48b1baf5d06f61546f8d60f7ae589f5c6ea6afab
Vishnu14052002 <vishnug14052002@gmail.com>
2025-10-20 12:43:47 +0100

    commits

--- leetcode problems/linked_list/206_reverse_linkedlist.py
+++ leetcode problems/linked_list/206_reverse_linkedlist.py
@@ -3,18 +3,49 @@ class Node():
         self.val = val
         self.next = None
     
-class LinkedList():
+class MyLinkedList():
     def __init__(self):
         self.head = None
 
-    def addAtEnd(self, val):
+    def addAtTop(self, val):
+        addAtTop = Node(val)
+        addAtTop.next = self.head
+        self.head = addAtTop
+
+    def addAtTail(self, val):
+        if self.head is None:
+            self.addAtTop(val)
+        
+        else:
+            addAtTail = Node(val)
+            s1 = self.head
+            while s1.next is not None:
+                s1 = s1.next
+            s1.next = addAtTail
+        
+    def addAtIndex(self, val):
+        if val < 0:
+            return
+        
+        if val == 0:
+            self.addAtTop(val)
+            return
+        
+        prev = self.head
         
 
     def traverse(self):
         s1 = self.head
-        while self.head is not None:
-            print(s1.value)
+        while s1 is not None:
+            print(s1.val)
             s1 = s1.next
     
 
 
+LL = MyLinkedList()
+LL.addAtTop(30)
+LL.addAtTop(20)
+LL.addAtTop(10)
+LL.addAtTail(100)
+LL.addAtTail(200)
+LL.traverse()
