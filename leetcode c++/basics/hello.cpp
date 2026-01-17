9aab0a1dd2935d03a335d376e7f592ac394af6dc
Vishnu14052002 <vishnug14052002@gmail.com>
2026-01-17 02:09:08 +0000

    b

--- leetcode c++/basics/hello.cpp
+++ leetcode c++/basics/hello.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main(){
     int num = 24;
-    cout << "hello vishnu go \n";
+    cout << "hello vishnu  \n";
     cout << "my age is " << num - 1 << endl;
     cout << num << endl;
     cout << num << "\n";
