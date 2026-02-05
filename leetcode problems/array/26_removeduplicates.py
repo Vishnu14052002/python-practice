a934aa7670cf80611608b95d5c387784093b8a55
Vishnu14052002 <vishnug14052002@gmail.com>
2026-02-05 14:48:08 +0000

    c

---
+++ leetcode problems/array/26_removeduplicates.py
@@ -0,0 +1,17 @@
+class Solution:
+    def removeDuplicates(self, nums):
+        x = 0
+        for i in range(len(nums)):
+            if nums[x] < nums[i]:
+                x += 1
+                nums[x] = nums[i]
+
+        return nums[:x+1]
+        
+
+
+
+nums = [0,0,1,1,1,2,2,3,3,4]
+obj = Solution()
+a = obj.removeDuplicates(nums)
+print(a)
