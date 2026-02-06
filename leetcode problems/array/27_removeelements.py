01db4b460d1625dee9d8d06d56de922e899fa7c2
Vishnu14052002 <vishnug14052002@gmail.com>
2026-02-06 12:52:11 +0000

    c

---
+++ leetcode problems/array/27_removeelements.py
@@ -0,0 +1,17 @@
+class Solution:
+    def removeElement(self, nums, val):
+        p1 = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[p1] = nums[i]
+                p1 += 1
+                
+        print(nums, p1)
+
+
+
+
+obj = Solution()
+val = 2
+nums = [0,1,2,2,3,0,4,2]
+obj.removeElement(nums, val)
