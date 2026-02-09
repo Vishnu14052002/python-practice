e1a06e190f5124f3c1c2b4762341843fceb16a85
Vishnu14052002 <vishnug14052002@gmail.com>
2026-02-09 15:27:51 +0000

    commits

--- leetcode problems/array/dummy.py
+++ leetcode problems/array/dummy.py
@@ -5,14 +5,14 @@ class Solution:
             if nums[i] != val:
                 nums[p1] = nums[i]
                 p1 += 1
-        print(nums[:p1])
+        print(nums)
 
 
-commits
+
+
 
 obj = Solution()
 val = 2
 nums = [0,1,2,2,3,0,4,2]
 obj.dummy(nums, val)
 
-''commits
