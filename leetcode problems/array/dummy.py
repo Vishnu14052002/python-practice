1bd389350da0a5bf1fec52b2c6390978ad67d6f0
Vishnu14052002 <vishnug14052002@gmail.com>
2026-02-10 15:05:57 +0000

    merg sorted array

--- leetcode problems/array/dummy.py
+++ leetcode problems/array/dummy.py
@@ -1,12 +1,27 @@
 class Solution:
-    def dummy(self, nums, val):
+    def dummy(self, nums1, nums2, m, n):
+        dnum1 = nums1
+        p1 = 0
+        p2 = 0
+        for i in range(len(nums1)):
+            if dnum1[p1] < nums2[p2]:
+                nums1[i] = dnum1[p1]
+                p1 += 1
+            
+            if dnum1[p1] > nums2[p2]:
+                nums1[i] = nums2[p2]
+                p2 += 1
 
+        print(nums1)
 
 
 
 
 obj = Solution()
-val = 2
-nums = [0,1,2,2,3,0,4,2]
-obj.dummy(nums, val)
+nums1 = [1,2,3,0,0,0]
+m = 3
+nums2 = [2,5,6]
+n = 3
+
+obj.dummy(nums1, nums2, m, n)
 
